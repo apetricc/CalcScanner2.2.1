641f03eadd218e89ce3eaf44024e30ccdf8ef3c9
Andrew P. <apetricc@unca.edu>
2015-09-25 11:53:23 -0400

    Update Tokens.java

--- Tokens.java
+++ Tokens.java
@@ -5,12 +5,11 @@ public interface Tokens {
 
     final int NUMBER = 500;
     final int ERROR = -1;
-    final static int ASSIGN = 501;
-    final int READ = 777;
-    final int WRITE = 8910;
+    final static int ASSIGN = 5555;
+    final int READ = 7777;
+    final int WRITE = 8888;
     final int ID = 333;
-    final int IDENTIFIER = 999;
-    final int DOUBLE_DOLLAR = 502;
+    final int DOUBLE_DOLLAR = 444;
     final int EOF = 600;
     final int COMMENT = 222;
 
