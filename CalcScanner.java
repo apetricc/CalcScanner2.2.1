3fea9f5ed0e906c0b401fc1b9a5a6193bf2fbd0d
Andrew P. <apetricc@unca.edu>
2015-09-25 11:52:49 -0400

    Update CalcScanner.java

--- CalcScanner.java
+++ CalcScanner.java
@@ -31,7 +31,7 @@ public class CalcScanner implements Tokens {
 
         int c = 0;
         StringBuilder lexeme = new StringBuilder("");
-        int c2;
+        //int c2;
         while (true) {
             c = pbReader.read();
             if (c == -1) {
@@ -54,12 +54,22 @@ public class CalcScanner implements Tokens {
             }
 
             if ((char) c == ':') {
-                    c2 = pbReader.read();
-                    if (c2 != '=') {
-                        return -1;
-                    }
-                    return ASSIGN;
+                lexeme.append((char)c);
+                c = pbReader.read();
+                if (c != '=') {
+                    return -1;
+                }
+                return ASSIGN;
             }
+            if ((char) c == '$') {
+                lexeme.append((char)c);
+                c = pbReader.read();
+                if (c != '$') {
+                    return -1;
+                }
+                return DOUBLE_DOLLAR;
+            }
+
 
             /**
              * if cur_char = '/'
@@ -67,6 +77,15 @@ public class CalcScanner implements Tokens {
             if (c == '/') {
                 String comment = "";
                 lexeme.append((char) c);
+/**
+            public void readComment() {
+                c = pbReader.read();
+                while ((char) c != '*') {
+                    lexeme.append((char) c);
+                    comment += lexeme;
+                    c = pbReader.read();
+                }
+**/
                 c = pbReader.read();
                 if (c == ' ') return '/';
                 if (c == '*') {
@@ -76,33 +95,29 @@ public class CalcScanner implements Tokens {
                         comment += lexeme;
                         c = pbReader.read();
                     }
-                    pbReader.unread(c);
-                    return COMMENT;
+                    c = pbReader.read();
+                    if (c == '/') return COMMENT;
+                    else {
+                       // readComment();
+                        return COMMENT;
+                    }
 
                 }
 
             }
-            /* 
-            here's the pseudoCode as Sheaffer explained it:
-            
-            c <-- read()
-            .
-            .
-            .
-            if(c is a letter) {
-              lexeme <-- c
-              c <-- read()
-              while(c is a letter or digit) {
-                lexeme += c
-                c <-- read()
-              }
-              unread(c)
-              if (lexeme.equals("read")) return READ
-              if (lexeme.equals("write")) return WRITE
-            else return ID 
-            
-            
-             * **/
+
+            if (Character.isDigit(c)) {
+                lexeme.append((char) c);
+                c = pbReader.read();
+                while(Character.isDigit(c)) {
+                    lexeme.append((char)c);
+                    c = pbReader.read();
+                }
+                pbReader.unread(c);
+                //if (Character.isLetter(c))
+                return NUMBER;
+            }
+
             if (Character.isLetter(c) || Character.isDigit(c)) {
                 lexeme.append((char) c);
                 c = pbReader.read();
@@ -119,8 +134,22 @@ public class CalcScanner implements Tokens {
             //if (lexeme.equals("write")) return WRITE;
             // System.out.println("Made it to point a");
             return -1;
+            }
+
         }
 
+
+    public int readIDs(StringBuilder lexeme, int c) throws IOException {
+        lexeme.append((char) c);
+        c = pbReader.read();
+        while(Character.isLetter(c) || Character.isDigit(c)) {
+            lexeme.append((char)c);
+            c = pbReader.read();
+        }
+        pbReader.unread(c);
+        if (lexeme.toString().equals("read")) return READ;
+        if (lexeme.toString().equals("write")) return WRITE;
+        return ID;
     }
 
     public static void main(String[] args) throws IOException {
